3f07a40ecf71772c7eb79d66ce09c19a5884326e
arhefr <efremenkoarhip614090@gmail.com>
2025-03-12 19:26:07 +0300

    rename logfile

--- calculator/cmd/server/start.go
+++ calculator/cmd/server/start.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile(".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("error open logfile", err)
 	}
