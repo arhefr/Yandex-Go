fbdbc6bb5d4a8f002610c02ba7037ce709e06bf6
arhefr <efremenkoarhip614090@gmail.com>
2025-03-13 23:32:50 +0300

    add errors

--- calculator/pkg/errors/errors.go
+++ calculator/pkg/errors/errors.go
@@ -5,6 +5,12 @@ import (
 )
 
 var (
-	ErrDivisionByZero = errors.New("error division by zero")
-	ErrIncorrectExpr  = errors.New("error incorrect math expression")
+	DivisionByZero = errors.New("error division by zero")
+	IncorrectExpr  = errors.New("error incorrect math expression")
+
+	IncorrectJSON = errors.New("error incorrect JSON")
+	IncorrectID   = errors.New("error incorrect ID")
+	NotFoundTask  = errors.New("error no available tasks")
+
+	CannotConnect = errors.New("error cannot connect to server")
 )
